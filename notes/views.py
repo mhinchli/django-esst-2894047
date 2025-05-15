31502a352dff78e9afcc2b9cc2e8416258108c86
mhinchli <martin.j.hinchliffe@Gmail.com>
2025-05-15 21:49:05 +0000

    Add all things to Git before putting my pc in sleep mode

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from django.views.generic import DetailView, ListView
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
-def detail(request, note_id):
-    note = Notes.objects.get(id=note_id)
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+
+class PopularNotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+    queryset = Notes.objects.filter(likes__gte=1).order_by('-likes')
+    
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note does not exist")
     return render(request, 'notes/notes_detail.html', {'note': note})
-#     def __str__(self):
